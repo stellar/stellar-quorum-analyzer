9d3497eb1071aab63a13b800891c80167454f44b
Jay Geng <jay@stellar.org>
2025-06-12 19:18:38 -0400

    Clean up tests, do not assert on model results. Remove dimacs files.

--- src/test/analyze.rs
+++ src/test/analyze.rs
@@ -1,6 +1,5 @@
 use crate::{FbasAnalyzer, FbasError, ResourceLimiter, SolveStatus};
 use std::collections::BTreeMap;
-use std::{io::BufRead, str::FromStr};
 
 fn assert_solver_limit_exceeded(res: Result<SolveStatus, FbasError>) -> bool {
     match res {
@@ -43,25 +42,16 @@ fn test_resource_limit() -> Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn test() -> Result<(), Box<dyn std::error::Error>> {
-    let expected_results: BTreeMap<&str, SolveStatus> = BTreeMap::from([
-        ("missing_1", SolveStatus::UNSAT),
-        ("circular_2", SolveStatus::UNSAT),
-        ("validators_broken_1", SolveStatus::UNSAT),
-        ("circular_1", SolveStatus::UNSAT),
-        ("top_tier", SolveStatus::UNSAT),
-        (
-            "conflicted_2",
-            SolveStatus::SAT((vec![2.into(), 3.into()], vec![0.into(), 1.into()])),
-        ),
-        ("homedomain_test_1", SolveStatus::UNSAT),
-        (
-            "conflicted_3",
-            SolveStatus::SAT((vec![1.into()], vec![0.into()])),
-        ),
-        (
-            "conflicted",
-            SolveStatus::SAT((vec![1.into(), 2.into()], vec![3.into(), 5.into()])),
-        ),
+    let expected_results: BTreeMap<&str, bool> = BTreeMap::from([
+        ("missing_1", false),
+        ("circular_2", false),
+        ("validators_broken_1", false),
+        ("circular_1", false),
+        ("top_tier", false),
+        ("conflicted_2", true),
+        ("homedomain_test_1", false),
+        ("conflicted_3", true),
+        ("conflicted", true),
     ]);
 
     for entry in std::fs::read_dir("./tests/test_data/")? {
@@ -69,27 +59,38 @@ fn test() -> Result<(), Box<dyn std::error::Error>> {
         if let Some(extension) = path.extension() {
             if extension == "json" {
                 let case_name = path.file_stem().unwrap().to_str().unwrap();
-                let expected = expected_results.get(case_name).expect(&format!(
+                let expected_sat = expected_results.get(case_name).expect(&format!(
                     "No expected result found for test case: {}",
                     case_name
                 ));
+
                 let mut solver = FbasAnalyzer::from_json_path(
                     path.as_os_str().to_str().unwrap(),
                     ResourceLimiter::unlimited(),
-                )
-                .unwrap();
-                let res = solver.solve()?;
+                )?;
 
-                match (&res, expected) {
-                    (SolveStatus::SAT((qa, qb)), SolveStatus::SAT((exp_qa, exp_qb))) => {
-                        assert_eq!(qa, exp_qa);
-                        assert_eq!(qb, exp_qb);
+                let res = solver.solve()?;
+                let actual_sat = matches!(res, SolveStatus::SAT(_));
+
+                assert_eq!(
+                    actual_sat, *expected_sat,
+                    "Case {} failed: expected {}, got {}",
+                    case_name, expected_sat, actual_sat
+                );
+
+                // Print the split if one was found
+                if actual_sat {
+                    let (qa, qb) = solver.get_potential_split()?;
+                    println!("\nFound quorum split for {}:", case_name);
+                    println!("Quorum A:");
+                    for validator in &qa {
+                        println!("  - {}", validator);
                     }
-                    (SolveStatus::UNSAT, SolveStatus::UNSAT) => {}
-                    _ => panic!(
-                        "Case {} failed: expected {:?}, got {:?}",
-                        case_name, expected, res
-                    ),
+                    println!("\nQuorum B:");
+                    for validator in &qb {
+                        println!("  - {}", validator);
+                    }
+                    println!();
                 }
             }
         }
@@ -99,56 +100,298 @@ fn test() -> Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn test_random_data() -> Result<(), Box<dyn std::error::Error>> {
-    let mut test_cases = vec![];
-    let dir_path = std::ffi::OsString::from_str("./tests/test_data/random/").unwrap();
+    let expected_results: BTreeMap<&str, bool> = BTreeMap::from([
+        ("almost_symmetric_network_10_orgs_", false),
+        ("almost_symmetric_network_12_orgs_", false),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_10",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_11",
+            true,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_2",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_3",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_4",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_5",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_6",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_7",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_8",
+            false,
+        ),
+        (
+            "almost_symmetric_network_12_orgs_delete_prob_factor_9",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_10",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_11",
+            true,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_12",
+            true,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_2",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_3",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_4",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_5",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_6",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_7",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_8",
+            false,
+        ),
+        (
+            "almost_symmetric_network_13_orgs_delete_prob_factor_9",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_10",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_11",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_12",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_13",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_2",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_3",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_4",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_5",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_6",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_7",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_8",
+            false,
+        ),
+        (
+            "almost_symmetric_network_14_orgs_delete_prob_factor_9",
+            false,
+        ),
+        ("almost_symmetric_network_16_orgs_", false),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_10",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_11",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_12",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_13",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_14",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_15",
+            true,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_2",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_3",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_4",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_5",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_6",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_7",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_8",
+            false,
+        ),
+        (
+            "almost_symmetric_network_16_orgs_delete_prob_factor_9",
+            false,
+        ),
+        ("almost_symmetric_network_2_orgs_", false),
+        (
+            "almost_symmetric_network_5_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_5_orgs_delete_prob_factor_2",
+            false,
+        ),
+        (
+            "almost_symmetric_network_5_orgs_delete_prob_factor_3",
+            false,
+        ),
+        ("almost_symmetric_network_5_orgs_delete_prob_factor_4", true),
+        (
+            "almost_symmetric_network_6_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_6_orgs_delete_prob_factor_2",
+            false,
+        ),
+        (
+            "almost_symmetric_network_6_orgs_delete_prob_factor_3",
+            false,
+        ),
+        (
+            "almost_symmetric_network_6_orgs_delete_prob_factor_4",
+            false,
+        ),
+        (
+            "almost_symmetric_network_6_orgs_delete_prob_factor_5",
+            false,
+        ),
+        ("almost_symmetric_network_8_orgs_", false),
+        (
+            "almost_symmetric_network_8_orgs_delete_prob_factor_1",
+            false,
+        ),
+        (
+            "almost_symmetric_network_8_orgs_delete_prob_factor_2",
+            false,
+        ),
+    ]);
+
     for entry in std::fs::read_dir("./tests/test_data/random/")? {
         let path = entry?.path();
         if let Some(extension) = path.extension() {
-            if extension == "dimacs" {
-                let case = path.file_stem().unwrap().to_os_string();
-                test_cases.push(case);
-            }
-        }
-    }
+            if extension == "json" {
+                let case_name = path.file_stem().unwrap().to_str().unwrap();
+                let expected_sat = expected_results.get(case_name).expect(&format!(
+                    "No expected result found for test case: {}",
+                    case_name
+                ));
 
-    for case in test_cases {
-        let mut json_file = dir_path.clone();
-        json_file.push(case.clone());
-        json_file.push(".json");
+                let mut solver = FbasAnalyzer::from_json_path(
+                    path.as_os_str().to_str().unwrap(),
+                    ResourceLimiter::unlimited(),
+                )?;
 
-        let mut dimacs_file = dir_path.clone();
-        dimacs_file.push(case.clone());
-        dimacs_file.push(".dimacs");
+                let res = solver.solve()?;
+                let actual_sat = matches!(res, SolveStatus::SAT(_));
 
-        let mut solver = FbasAnalyzer::from_json_path(
-            json_file.as_os_str().to_str().unwrap(),
-            ResourceLimiter::unlimited(),
-        )
-        .unwrap();
-        let res = solver.solve()?;
-        {
-            // Open and read the file line by line
-            let file = std::fs::File::open(dimacs_file).expect("Failed to open the DIMACS file");
-            let reader = std::io::BufReader::new(file);
+                assert_eq!(
+                    actual_sat, *expected_sat,
+                    "Case {} failed: expected {}, got {}",
+                    case_name, expected_sat, actual_sat
+                );
 
-            // Look for the result comment line
-            let mut expected = false;
-            for line in reader.lines() {
-                let line = line.expect("Failed to read line");
-                if line.starts_with("c") {
-                    if line.contains("UNSATISFIABLE") {
-                        expected = false;
-                        break;
-                    } else if line.contains("SATISFIABLE") {
-                        expected = true;
-                        let (qa, qb) = solver.get_potential_split().unwrap();
-                        println!("quorum a: {:?}, quorum b: {:?}", qa, qb);
-                        break;
+                // Print the split if one was found
+                if actual_sat {
+                    let (qa, qb) = solver.get_potential_split()?;
+                    println!("\nFound quorum split for {}:", case_name);
+                    println!("Quorum A:");
+                    for validator in &qa {
+                        println!("  - {}", validator);
                     }
+                    println!("\nQuorum B:");
+                    for validator in &qb {
+                        println!("  - {}", validator);
+                    }
+                    println!();
                 }
             }
-            let is_sat = matches!(res, SolveStatus::SAT(_));
-            assert_eq!(is_sat, expected);
         }
     }
     Ok(())
